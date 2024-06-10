6d138aa425e05828acaaaddaada23e3a8382e74d
mungg <sigma89kim@gmail.com>
2024-06-10 02:19:45 -0400

    update version and add few-shot examples

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name="VeriScore",
-   version="0.0.4",
+   version="0.1.0",
    packages=find_packages(),
    install_requires=[
        #'requests', 'numpy'
